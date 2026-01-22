6f467f3762b6e6cc056dff7d91353fddc524547c
João Vieira <joaofilipevieira1998@gmai.com>
2026-01-22 23:43:14 +0000

    Add GetReportData method and define FREE_API_URL constant in Loader class

--- OgameGenSim/Loader.cs
+++ OgameGenSim/Loader.cs
@@ -4,5 +4,12 @@ namespace OgameGenSim;
 
 public class Loader
 {
+    public const string FREE_API_URL = "https://ogapi.faw-kes.de/v1/report/" ;
 
+    public bool GetReportData(string espionageId)
+    {
+        
+
+        return true;
+    }
 }
