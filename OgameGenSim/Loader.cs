30170b2dfd489a4d38db617816503003dfcdf0cf
João Vieira <joaofilipevieira1998@gmai.com>
2026-01-26 22:10:32 +0000

    Refactor Loader class to support asynchronous report retrieval and enhance error handling

--- OgameGenSim/Loader.cs
+++ OgameGenSim/Loader.cs
@@ -1,15 +1,71 @@
 using System;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using SimulatorPack;
 
 namespace OgameGenSim;
 
-public class Loader
+public class Loader(HttpClient client)
 {
     public const string FREE_API_URL = "https://ogapi.faw-kes.de/v1/report/" ;
+    private HttpClient Client { get; set; } = client;
 
-    public bool GetReportData(string espionageId)
+    public async Task<EspionageReport> GetReportDataAsync(string espionageId)
     {
-        
+        var espionageResult = await Client.GetAsync(FREE_API_URL + espionageId);
 
-        return true;
+        try
+        {
+            espionageResult.EnsureSuccessStatusCode();
+
+        }
+        catch (HttpRequestException e)
+        {
+            return new EspionageReport
+            {
+                StatusCode = e.StatusCode ?? HttpStatusCode.InternalServerError,
+                Message = e.Message
+            };
+
+        }
+        var jsonString = await espionageResult.Content.ReadAsStringAsync();
+        var espionageReport = JsonSerializer.Deserialize<EspionageReport>(jsonString);
+
+        return  espionageReport ?? new EspionageReport
+        {
+            StatusCode = HttpStatusCode.InternalServerError,
+            Message = "Failed to deserialize espionage report"
+        };
     }
+}
+
+public class EspionageReport
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public required string Message { get; set; }
+
+    [JsonPropertyName("RESULTDATA.details.combatInformation")]
+    public CombatInformation CombatInformation { get; set; }
+}
+
+public class CombatInformation
+{
+    //TODO: Change Type to something usefull
+    [JsonPropertyName("coords")]
+    public string Coordinates { get; set; } 
+    public PlayerClass CharacterClassId { get; set; }
+    public AllianceClass AllianceClassId { get; set; }
+
+}
+
+//TODO: Implement Researches
+public class Researches
+{
+    
+}
+
+public enum ResearchEnum {
+    
 }
